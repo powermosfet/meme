053252222e02f5541861afb8e4ddace83e823822
Asmund Berge <asmund@berge.id>
2024-06-30 21:06:26 +0200

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> { }; # pin the channel to ensure reproducibility!
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") { }; # pin the channel to ensure reproducibility!
   # Add this if you are building a devShell in a flake. Usually, it's auto-detected
   # using lib.inNixShell, but that doesn't work in flakes
   # returnShellEnv = true;
