0c90a3ddbefa0b7fcc1f6ca12ca9ddfa9de862c7
Asmund Berge <asmund@berge.id>
2024-07-01 19:31:53 +0200

    Make default.nix a function

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
-let
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") { }; # pin the channel to ensure reproducibility!
-  # Add this if you are building a devShell in a flake. Usually, it's auto-detected
-  # using lib.inNixShell, but that doesn't work in flakes
-  # returnShellEnv = true;
-in
+{ pkgs ? import <nixpkgs> { } }:
+
+# let
+#   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") { }; # pin the channel to ensure reproducibility!
+#   # Add this if you are building a devShell in a flake. Usually, it's auto-detected
+#   # using lib.inNixShell, but that doesn't work in flakes
+#   # returnShellEnv = true;
+# in
 pkgs.haskellPackages.developPackage {
   root = ./.;
 }
