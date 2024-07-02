223357e2922942d92d1bb7b662c7f195358a828a
Asmund Berge <asmund@berge.id>
2024-07-02 20:16:23 +0200

    Add SELECT statement  during startup

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,15 @@ module Main where
 import Network.URI (parseURI)
 import Network.MQTT.Client
 import qualified Config
+import Database.PostgreSQL.Simple
 
 main :: IO ()
 main = do
   config <- Config.create
   mc <- connectURI mqttConfig{_msgCB=SimpleCallback msgReceived} (Config.mqttUri config)
+  conn <- connectPostgreSQL ""
+  [Only (eventId::Int, _, group::Text, event::Text, _)] <- query_ conn "SELECT * FROM event LIMIT 1"
+  print (eventId, group, event)
   publish mc "tmp/topic" "hello!" False
   print =<< subscribe mc [("event/+/+", subOptions)] []
   waitForClient mc   -- wait for the the client to disconnect
